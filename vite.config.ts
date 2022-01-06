79e872fe5463eea7b61da39a76bb435453028f84
Rody Davis <rody.davis.jr@gmail.com>
2022-01-05 20:08:38 -0800

    updating demo

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    base: "/lit-file-based-routing/",
     rollupOptions: {
       input: "index.html",
     },
